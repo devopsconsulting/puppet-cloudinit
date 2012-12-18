c2c770b266db4805f395a15aca2a72b9d509fcb4
Martijn Jacobs <martijn@fourdigits.nl>
2012-12-18 14:40:09 +0000

    ensure "present" should be ensure "file"

--- manifests/disablemanagehosts.pp
+++ manifests/disablemanagehosts.pp
@@ -1,6 +1,6 @@
 class cloudinit::disablemanagehosts {
      file { "/etc/cloud/cloud.cfg.d/99_manage_etc_hosts.cfg":
          source => "puppet:///modules/cloudinit/99_manage_etc_hosts.cfg",
-         ensure => "present",
+         ensure => "file",
      }    
 }
